b8d1e1f9530fedce24fc3c6478659f609d9e81d8
mrohmer <rohmer.matze@gmail.com>
2021-08-15 22:58:44 +0200

    patch(data): complete list

--- src/data/list/default/index.ts
+++ src/data/list/default/index.ts
@@ -1,14 +1,36 @@
-import important from './important.yaml'
-import forTheRide from './for-the-ride.yaml'
-import clothes from './clothes.yaml'
+import important from './important.yaml';
+import forTheRide from './for-the-ride.yaml';
+import clothes from './clothes.yaml';
+import mobileHome from './mobile-home.yaml';
+import spongeBag from './sponge-bag.yaml';
+import food from './food.yaml';
+import boat from './boat.yaml';
+import toolBox from './tool-box.yaml';
+import diverse from './diverse.yaml';
+import carBeforeDeparture from './car-before-departure.yaml';
+import trailerBeforeDeparture from './trailer-before-departure.yaml';
+import boatBeforeDeparture from './boat-before-departure.yaml';
+import diverseBeforeDeparture from './diverse-before-departure.yaml';
 import type {IList} from '../../../model/list';
+
+const version = 1;
 
 export const defaultList: IList = {
   label: 'default',
-  key: 'Packliste',
+  key: `Packliste.v${version}`,
   groups: [
     important,
     forTheRide,
     clothes,
+    mobileHome,
+    spongeBag,
+    food,
+    boat,
+    toolBox,
+    diverse,
+    carBeforeDeparture,
+    trailerBeforeDeparture,
+    boatBeforeDeparture,
+    diverseBeforeDeparture,
   ]
 }

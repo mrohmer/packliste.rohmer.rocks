7e1f718b1456d6b558b8737cddff3c6b5aafbc3a
mrohmer <rohmer.matze@gmail.com>
2023-10-06 17:54:07 +0200

    feat: add page transitions

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -2,11 +2,17 @@
 	export let withName = true;
 </script>
 
-<div class="flex items-center gap-5">
-	<img class="block invert h-8 w-8 transition-all" src="/icons/icon_100.png" alt="checkbox" />
+<div class="root flex items-center gap-5 w-fit">
+	<img class="logo block invert h-8 w-8 transition-all" src="/icons/icon_100.png" alt="checkbox" />
 	{#if withName}
-		<div>
+		<div class="claim">
 			<h1 class="text-3xl font-extralight">Packliste</h1>
 		</div>
 	{/if}
 </div>
+
+<style lang="postcss">
+	.root {
+		view-transition-name: logo;
+	}
+</style>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-	import '../app.pcss';
+	import '../../app.pcss';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate(() => {
+		if (!('startViewTransition' in document)) return;
+		return new Promise((fulfil) => {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			(document as any).startViewTransition(() => new Promise(fulfil));
+		});
+	});
 </script>
 
 <slot />

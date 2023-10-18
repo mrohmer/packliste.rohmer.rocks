36049006b16347fb58739827a07faeb274678de4
mrohmer <rohmer.matze@gmail.com>
2023-10-18 21:35:52 +0200

    fix: fix build issues

--- src/routes/(app)/(authenticated)/api/templates/+server.ts
+++ src/routes/(app)/(authenticated)/api/templates/+server.ts
@@ -2,5 +2,4 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 import { readListFiles } from '$lib/server/content/read-content';
 
-export const prerender = true;
 export const GET: RequestHandler = async () => json((await readListFiles()) ?? []);
